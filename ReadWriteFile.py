7a794e5992e24fa7972fe1171d75eb773bc8f325
Sachin <jakhotiya.sachin@gmail.com>
2020-05-02 12:43:54 -0700

    ReadWriteFile program

--- ReadWriteFile.py
+++ ReadWriteFile.py
@@ -1,5 +1,13 @@
 file = open('test.txt')
 
-print(file.read(10))
-print(file.readline())
+#print(file.read(10))
+#print(file.readline())
+
+line = file.readline()
+
+while line != '':
+    print(line)
+    line = file.readline()
+
+
 file.close()
